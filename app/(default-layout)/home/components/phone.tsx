2b6eea6957520bc8087d6e58f1f2515a69451d13
tony <tony@playzoey.com>
2025-06-17 02:36:36 +0900

    [디자인] 수정

--- app/(default-layout)/home/components/phone.tsx
+++ app/(default-layout)/home/components/phone.tsx
@@ -1,13 +1,19 @@
 export default function Phone() {
   return (
-    <div className="phone">
-      <div className="notch"></div>
-      <div className="screen">
-        <div className="content">
-          <h2>Hello, iPhone 15!</h2>
-          <p>This is a sample screen content.</p>
+    <section className="phone">
+      <h2 className="hidden">Phone</h2>
+      <div className="top">
+        <div className="agency">SKT</div>
+        <div className="notch"></div>
+        <div className="status">
+          <span>상태창</span>
+          <span>wifi</span>
+          <span>배터리</span>
         </div>
       </div>
-    </div>
+      <div className="screen">
+        <div className="content"></div>
+      </div>
+    </section>
   );
 }
